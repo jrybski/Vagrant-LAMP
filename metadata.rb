6718606bde69172dc16890d7e8085ed1006ec771
Janusz Rybski <janusz.rybski@schibsted.pl>
2014-10-06 23:51:52 +0200

    gitignore and metadata.rb for compability purpose

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ads'
+maintainer       'jrybski'
+maintainer_email 'janusz@rybski.pl'
+license          'All rights reserved'
+description      'Installs/Configures ads app'
+long_description 'Installs/Configures ads app'
+version          '0.9.0'
+
